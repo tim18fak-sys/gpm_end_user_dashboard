f22a33e94f572e02984061ef9fa8fdde433a2012
tim18fak-sys <tim.jobs.avell@gmail.com>
2026-03-02 19:43:05 -0500

    starting the onboarding flow modal implementation

--- src/wrapper/OnboardingFlowWrapper.tsx
+++ src/wrapper/OnboardingFlowWrapper.tsx
@@ -1,7 +1,9 @@
+import { useAuthStore } from "@/store/authStore"
 import { PropsWithChildren } from "react"
 
 interface OnboardingFlowWrapperProps extends PropsWithChildren{}
 const OnboardingFlowWrapper = (props: OnboardingFlowWrapperProps) => {
+    const { leadBoardingFlow } = useAuthStore((state) => state.user)
   return (
     <div>
       {props.children}
