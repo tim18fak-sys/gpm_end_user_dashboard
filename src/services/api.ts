545f66d2f1cb46eb419d234b29335ed4040ee7c8
tim18fak-sys <tim.jobs.avell@gmail.com>
2026-07-27 17:08:44 -0400

    added the .env file setup

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,8 +4,9 @@ import { useAuthStore } from '../store/authStore'
 import { BaseDataInterface } from "@/types/shared";
 import { User } from "@/types/user.types";
 
-// const API_BASE_URL = 'https://campus-pal-monolith-api.onrender.com' // Replace with your actual API URL
- const API_BASE_URL =  'http://localhost:4000' // Use environment variable or fallback to localhost
+
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:4000";
 export const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
